abc0fa30019c916a155295265a99dbab692368f1
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-05-04 16:50:24 -0300

    Redoing the project to make it more modular and better.

---
+++ square.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three'
+
+class notation{
+  constructor(column, row){
+    return {column, row}
+  }
+}
+
+class square{
+  constructor(column, row, is_light, object, translate){
+    this.square_notation = new notation(column,row)
+    this.is_light = is_light
+    this.object = object
+    const {x,z} = translate(column, row)
+    this.object.position.x = x
+    this.object.position.z = z
+    this.object.name = column+row
+  }
+}
+
+export {
+  notation,
+  square
+}
