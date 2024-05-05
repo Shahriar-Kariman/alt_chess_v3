d9069cf7e3801dba3ad2735d7d2f201195c90b9a
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-05-04 23:51:04 -0300

    defined the skeleton for pieces.

---
+++ pieces.js
@@ -0,0 +1,22 @@
+import * as THREE from 'three'
+
+class pieces{
+  constructor(){
+    this.pieceList = []
+    this.pieces = new THREE.Group()
+  }
+  checkSquare(square){
+    const {column, row} = square
+    let piece
+    this.pieceList.forEach(
+      (p) => {
+        if(p.current_square.column===column && p.current_square.row==row){
+          piece = p
+        }
+      }
+    )
+    return piece  
+  }
+}
+
+export default pieces
