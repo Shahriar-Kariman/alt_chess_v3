a937595165898edc388bb14e63480632a86f7391
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-05-10 14:10:37 -0300

    I made it so it is possible to capture with pawns.

--- pieces/pawn.js
+++ pieces/pawn.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import piece from '../piece'
 import { darkPieceMaterial, lightPieceMaterial, pawnGeometry } from '../global'
 import { notation } from '../square'
+import { b1_pieces } from '../chess_experiance'
 
 class pawn extends piece{
   constructor(is_light, start_square){
@@ -21,15 +22,35 @@ class pawn extends piece{
     const {column, row} = this.current_square
     const currentColCharCode = column.charCodeAt(0)
     const direction = this.is_light ? 1 : -1
-    const squares = [new notation(column, row+1*direction)]
-    if(this.move_count==0){
-      squares.push(new notation(column,row+2*direction))
+    const squares = []
+    let p = b1_pieces.checkSquare(column, row+1*direction)
+    let blocked = true
+    if(p===undefined) {
+      squares.push(new notation(column, row+1*direction))
+      blocked = false
     }
-    // consider saptures and special moves later
+    // first move
+    if(this.move_count==0 && !blocked){
+      p = b1_pieces.checkSquare(column,row+2*direction)
+      if(p===undefined) squares.push(new notation(column,row+2*direction))
+    }
+    // captures
+    p = b1_pieces.checkSquare(String.fromCharCode(currentColCharCode+1),row+1*direction)
+    if(p!==undefined)
+      squares.push(
+        new notation(String.fromCharCode(currentColCharCode+1),row+1*direction)
+      )
+    p = b1_pieces.checkSquare(String.fromCharCode(currentColCharCode-1),row+1*direction)
+    if(p!==undefined)
+      squares.push(
+        new notation(String.fromCharCode(currentColCharCode-1),row+1*direction)
+      )
+    // en passant
     return squares
   }
   updateLegalMoves(){
     this.legal_moves = this.getLegalMoves()
+    console.log(this.legal_moves)
   }
 }
 
